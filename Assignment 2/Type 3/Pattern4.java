635c06ac18fb9018985d4bc8fe239bb54dcce707
mayurc16 <113438617+mayurc16@users.noreply.github.com>
2022-09-25 12:42:53 +0530

    Add files via upload

---
+++ Assignment 2/Type 3/Pattern4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Pattern4{
+	public static void main(String args[]){
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter number of rows:");
+	int n=sc.nextInt();
+	
+	for(int i=1; i<=n; i++)
+	{
+		for(int j=n;j>=i;j--)
+		{	System.out.print("  ");
+		}
+		
+		for(int j=1;j<i;j++)
+		{	System.out.print(j+" ");
+		}
+		
+		for(int j=i;j>=1;j--)
+		{	System.out.print(j+" ");
+		}
+		
+		
+	System.out.println();
+	}
+	
+	}
+}
